6bbdd121d50c3d03929d74a56cce1715d789b05f
Caroll1889 <diahancaroll@hotmail.com>
2020-01-03 20:40:36 +0000

    web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env bash
+#script that sets up the web servers for the deployment of web_static
+
+apt-get -y update
+apt-get -y install nginx
+mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+echo "test" | tIee -a /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sed -ie "45i\\tlocation / {\\n\\t alias /data/web_static/current/;/" /etc/nginx/sites-available/default
+service ngnix restart
