b90cfdf143f5d6048c2d91cd8e53f73ab8d993bd
Caroll1889 <diahancaroll@hotmail.com>
2020-01-10 00:48:52 +0000

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# Script that generates a .tgz archive
+
+import os
+from datetime import datetime
+from fabric.operations import local, run, put, env
+env.hosts = ['34.74.218.90', '35.229.122.165']
+
+
+def do_deploy(archive_path):
+
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        base = os.path.basename('archive_path')
+        os.path.splitext(base)
+        fileEx = os.path.splitext(base)[0]
+        file2 = "/data/web_static/releases/"
+        file1 = "tmp/web_static_20170315003959.tgz"
+        put(archive_path, '/tmp/')
+        run('mkdir -p /data/web_static/release/{}'.format(fileEx))
+        run('tar -xzf {} -C {}{}'.format(file1, file2, fileEx))
+        run('rm {}'.format(file1))
+        run('mv {}{}/web_static/* {}{}'.format(file2, fileEx, file2, fileEx))
+        run('rm -rf {}{}/web_static'.format(file2, fileEx))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{} /data/web_static/current'.format(file2, fileEx))
+        print("New version deployed!")
+        return True
+
+    except Exception:
+        return False
